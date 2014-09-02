707ef3b25d855964a63ecbac2857c2dd0b8c7462
Oli Lane <ojgl2@cam.ac.uk>
2014-09-02 02:45:09 +0100

    Add member count to societies

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 angular.module('socAlloc', [])
   .controller('AllocController', ['$scope', function($scope) {
     $scope.socs = [
-      {name:'Football', requestedFunding:500, allocatedFunding:0},
-      {name:'Rowing', requestedFunding:400, allocatedFunding:0}];
+      {name:'Football',
+       requestedFunding:500,
+       memberCount:23,
+       allocatedFunding:20},
+      {name:'Rowing',
+       requestedFunding:400,
+       memberCount:13,
+       allocatedFunding:50}];
 
     $scope.addSoc = function() {
-      $scope.socs.push({name:"", requestedFunding:0, allocatedFunding:0});
+      $scope.socs.push({name:"",
+                        requestedFunding:0,
+                        memberCount:0, 
+                        allocatedFunding:0});
     };
 
 
