c7b253a5940d2cd2fba908d00481d8aa763a9009
Oli Lane <ojgl2@cam.ac.uk>
2014-09-02 02:39:05 +0100

    Can remove rows, and get a total count of requested funding

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 angular.module('socAlloc', [])
   .controller('AllocController', ['$scope', function($scope) {
     $scope.socs = [
-      {name:'Football', requested:500},
-      {name:'Rowing', requested:400}];
+      {name:'Football', requestedFunding:500, allocatedFunding:0},
+      {name:'Rowing', requestedFunding:400, allocatedFunding:0}];
 
     $scope.addSoc = function() {
-      $scope.socs.push({name:"", requested:0});
+      $scope.socs.push({name:"", requestedFunding:0, allocatedFunding:0});
+    };
+
+
+    $scope.rmSoc = function() {
+      $scope.socs.pop();
+    };
+
+    $scope.totalRequested = function(){
+      var count = 0;
+      angular.forEach($scope.socs, function(soc) {
+        count += soc.requestedFunding;
+      });
+      return count;
     };
   }]);
