788c6ccbb110b4fc2e35896162523a4b97ef3e14
Oli Lane <ojgl2@cam.ac.uk>
2014-09-02 12:34:02 +0100

    Added basic calculation logic

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ angular.module('socAlloc', [])
     $scope.addSoc = function() {
       $scope.socs.push({name:"",
                         requestedFunding:0,
-                        memberCount:0, 
+                        memberCount:0,
                         allocatedFunding:0});
     };
 
@@ -29,4 +29,65 @@ angular.module('socAlloc', [])
       });
       return count;
     };
+
+    $scope.socAllocation = function(soc){
+      $scope.calculate();
+      return soc.allocatedFunding;
+    };
+
+    $scope.calculate = function() {
+
+      var remainingBudget = $scope.budget;
+
+      angular.forEach($scope.socs, function(soc){
+        soc.allocatedFunding = 0;
+      });
+
+      while(remainingBudget > 0){
+
+        var totalUnfulfilledMembers = $scope.totalUnfulfilledMembers();
+
+        if(totalUnfulfilledMembers === 0){
+          break;
+        }
+
+        var budgetPerUnfulfilledMember = remainingBudget / totalUnfulfilledMembers;
+
+        var overflowBudget = 0;
+
+        angular.forEach($scope.socs, function(soc){
+          if(soc.allocatedFunding !== soc.requestedFunding){
+
+            soc.allocatedFunding += budgetPerUnfulfilledMember * soc.memberCount;
+
+            if(soc.allocatedFunding > soc.requestedFunding){
+              overflowBudget += soc.allocatedFunding - soc.requestedFunding;
+              soc.allocatedFunding = soc.requestedFunding;
+            }
+          }
+        });
+
+        remainingBudget = overflowBudget;
+
+      }
+    };
+
+    $scope.totalMembers = function() {
+      var count = 0;
+      angular.forEach($scope.socs, function(soc) {
+        count += soc.memberCount;
+      });
+      return count;
+    };
+
+    $scope.totalUnfulfilledMembers = function() {
+      var count = 0;
+      angular.forEach($scope.socs, function(soc) {
+        if(soc.requestedFunding !== soc.allocatedFunding){
+          count += soc.memberCount;
+        }
+      });
+      return count;
+    };
+
   }]);
